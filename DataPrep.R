0ef7f7f6cc67cb69e4b137ea380d71890085be8f
AngelComposer <kchen13@uw.edu>
2022-02-23 21:44:14 -0800

    Update DataPrep.R

--- DataPrep.R
+++ DataPrep.R
@@ -1,4 +1,7 @@
 library(dplyr)
+library(ggplot2)
 
 heartdisease <- read.csv("./data/heart.csv")
 nutrition_health <- read.csv("./data/National_Nutrition_Health.csv")
+
+death <- ggplot(geom_point(data = heartdisease, aes(x = "i..age", y = "chol")))

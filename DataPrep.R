80ca735ac51289c21379adcc4c35ebc9d61bf9d1
Minttu Welandgit config --global user.name Minttu <minttu@uw.edu>
2022-03-07 20:02:30 -0800

    age stats

--- DataPrep.R
+++ DataPrep.R
@@ -40,16 +40,39 @@ hypertension <- nutrition_health %>%
 hypertension <- hypertension %>%
   select(3,4,7)
 
+#age-standardized
 hypertension_age_standardized <- hypertension %>%
   filter(hypertension$Data_Value_Type == "Age-Standardized") %>%
-  select(1,3) %>%
-  group_by(hypertension$Indicator) %>%
-  aggregate(by = list(hypertension$Indicator),
-            FUN = sum)
-  
+  select(1,3) 
+
+hypertension_age_standardized <- hypertension_age_standardized %>%
+  group_by(hypertension_age_standardized$Indicator) %>%
+  summarize(mean = mean(hypertension_age_standardized$Data_Value_Alt))
+
+#crude
 hypertension_crude <-  hypertension %>%
   filter(hypertension$Data_Value_Type == "Crude")%>%
-  select(1,3) %>% 
-  group_by(hypertension$Indicator) %>%
-  aggregate(by = list(hypertension$Indicator),
-            FUN = sum)
+  select(1,3) 
+
+hypertension_crude <- hypertension_crude %>% 
+  group_by(hypertension_crude$Indicator) %>%
+  summarize(mean = mean(hypertension_crude$Data_Value_Alt))
+
+
+#hypertension stats
+hypertensions_stats <- nutrition_health %>% 
+  filter(nutrition_health$Topic == "Hypertension") %>%
+  select(3,7:9)
+
+hypertensions_stats <- hypertensions_stats %>%
+  filter(hypertensions_stats$Indicator == "Prevalence of hypertension among US adults (18+); NHANES") %>%
+  select(2:4)
+
+#age 
+hypertensions_stats_age <- hypertensions_stats %>%
+  filter(hypertensions_stats$Break_Out_Category == "Age") %>%
+  select(1,3)
+
+hypertensions_stats_age <- hypertensions_stats_age %>%
+  group_by(hypertensions_stats_age$Break_Out) %>%
+  summarize(mean = mean(hypertensions_stats_age$Data_Value_Alt))

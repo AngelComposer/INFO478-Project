d7d26938f92b2877f8a5e063f0d19994ebbfe970
Vivian Chien <>
2022-03-10 14:27:51 -0800

    added placeholders

--- app_server.R
+++ app_server.R
@@ -1,4 +1,36 @@
 # data
 
 server <- function(input, output) {
+  # graph 1
+  #   output$graph <- renderPlotly({
+  #     graph <- ggplot(data) +
+  #       geom_polygon(
+  #         mapping = aes_string(x = "", y = "", group = "",
+  #                              fill = input$(from widget)),
+  #         color = "", size = ) +
+  #       ggtitle("title") +
+  #       blank_theme +
+  #       easy_center_title() +
+  #     
+  #     ggplotly(graph)
+  #  })
+
+  # graph 2
+  #   output$graph <- renderPlotly({
+  #     graph <- ggplot(data) +
+  #       geom_polygon(
+  #         mapping = aes_string(x = "", y = "", group = "",
+  #                              fill = input$(from widget)),
+  #         color = "", size = ) +
+  #       ggtitle("title") +
+  #       blank_theme +
+  #       easy_center_title() +
+  #     
+  #     ggplotly(graph)
+  #  })
+
+# other values
+  # output$variable_name <- renderText({
+  #   return(variable_name)
+  # })
 }
--- app_ui.R
+++ app_ui.R
@@ -1,30 +1,78 @@
 # data
 
 # widgets
+chart_widget <- selectInput(
+  inputId = "variable_name",
+  label = "label",
+  # choices = list of choices
+)
+
+emission_type <- radioButtons(
+  inputId = "variable_name",
+  label = "label",
+  # choices = list of choices
+)
 
 # pages
 overview_page <- tabPanel(
   "Overview",
   
-  titlePanel("Introduction")
+  titlePanel("Introduction"),
+  
+  mainPanel(
+    p("paragraph 1"),
+    p("paragraph 2")
+  )
 )
 
 interactive_pg_1<- tabPanel(
   "Model 1",
   
-  titlePanel("Insert Title")
+  titlePanel("Insert Title"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      # widgets
+    ),
+  
+    mainPanel(
+      # graphs
+      # plotlyOutput(outputId = "")
+      p("paragraph 1"),
+      p("paragraph 2")
+    )
+  )
 )
 
 interactive_pg_2 <- tabPanel(
   "Model 2",
   
-  titlePanel("insert Title")
+  titlePanel("insert Title"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      # widgets
+    ),
+    
+    mainPanel(
+      # graphs
+      # plotlyOutput(outputId = "")
+      p("paragraph 1"),
+      p("paragraph 2")
+    )
+  )
 )
 
 analysis_conclusion <- tabPanel(
   "Analysis and Conclusions",
   
-  titlePanel("Analysis and Conclusions")
+  titlePanel("Analysis and Conclusions"),
+  
+  mainPanel(
+    
+    p("paragraph 1"),
+    p("paragraph 2")
+  )
 )
 
 ui <- navbarPage(
